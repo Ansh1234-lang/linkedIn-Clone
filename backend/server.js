5dedb5c6eb1a519e6422547f0a62ebbf3e140de9
unknown <ansh.verma.av0608@email.com>
2026-05-23 11:22:13 +0530

    initial commit - linked in clone setup

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const URI = process.env.MONGO_URI;
+// console.log(URI)
+const start = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log("MongoDB Error:", error);
+  }
+};
+
+start();
